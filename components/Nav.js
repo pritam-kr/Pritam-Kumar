4eb2d1e2d03f6e2fa359a61f97fa14cc98302cf6
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-09-20 15:52:27 +0530

    Initial commit

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/Nav.module.css"
+import "../styles/Nav.module.css"
+
+ 
+
+const Nav = () => {
+  return <nav className="nav">
+<h1>Hello</h1>
+  </nav>;
+};
+
+export { Nav };
---
+++ pages/project.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>Project</div>
+  )
+}
+
+export {Project}
